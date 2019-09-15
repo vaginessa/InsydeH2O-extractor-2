d7e1a3a7a44fe330c8649d3e7aa3623756f6c83d
gen BTC <genBTC@gmx.com>
2019-09-15 05:24:57 -0400

    version 0.3+genBTC - extract platforms.ini and EC.bin

--- extractor.c
+++ extractor.c
@@ -1,7 +1,11 @@
-/*  extractor.c - InsydeFlash BIOS image extractor
-    Author: Nikolaj Schlej 
+/*  extractor.c - InsydeFlash BIOS image extractor v0.2
+    Author: Nikolaj Schlej
     License: WTFPL
+    Modified: genBTC , 9/15/2019 
+    - v0.3 (adds additional extractions and changes command line arguments)
 */
+
+#define PROGRAM_NAME "InsydeFlashExtractor v0.3+genBTC"
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -28,6 +32,38 @@ typedef struct _IFLASH_BIOSIMG_HEADER {
     uint32_t FullSize;
     uint32_t UsedSize;
 } IFLASH_BIOSIMG_HEADER;
+
+const uint8_t IFLASH_INI_IMG_SIGNATURE[] = {
+    0x24, 0x5F, 0x49, 0x46, 0x4C, 0x41, 0x53, 0x48, 0x5F, 0x49, 0x4E, 0x49, 
+    0x5F, 0x49, 0x4D, 0x47
+}; // "$_IFLASH_INI_IMG"
+#define IFLASH_INI_IMG_SIGNATURE_LENGTH 16
+
+typedef struct _IFLASH_INI_IMG_HEADER {
+    uint8_t  Signature[IFLASH_INI_IMG_SIGNATURE_LENGTH];
+    uint32_t FullSize;
+    uint32_t UsedSize;
+} IFLASH_INI_IMG_HEADER;
+
+const uint8_t IFLASH_EC_IMG_SIGNATURE[] = {
+    0x24, 0x5F, 0x49, 0x46, 0x4C, 0x41, 0x53, 0x48, 0x5F, 0x45, 0x43, 0x5F, 
+    0x49, 0x4D, 0x47, 0x5F,
+}; // "$_IFLASH_EC_IMG_"
+#define IFLASH_EC_IMG_SIGNATURE_LENGTH 16
+
+typedef struct _IFLASH_EC_IMG_HEADER {
+    uint8_t  Signature[IFLASH_EC_IMG_SIGNATURE_LENGTH];
+    uint32_t FullSize;
+    uint32_t UsedSize;
+} IFLASH_EC_IMG_HEADER;
+
+// also exists: 
+//
+// _IFLASH_DRV_IMG 
+// 0x24, 0x5F, 0x49, 0x46, 0x4C, 0x41, 0x53, 0x48, 0x5F, 0x44, 0x52, 0x56, 0x5F, 0x49, 0x4D, 0x47, 
+// _IFLASH_BIOSCER
+// 0x24, 0x5F, 0x49, 0x46, 0x4C, 0x41, 0x53, 0x48, 0x5F, 0x42, 0x49, 0x4F, 0x53, 0x43, 0x45, 0x52,
+
 
 /* Implementation of GNU memmem function using Boyer-Moore-Horspool algorithm
 *  Returns pointer to the beginning of found pattern or NULL if not found */
@@ -74,12 +110,15 @@ int main(int argc, char* argv[])
     uint8_t* found;
     long filesize;
     long read;
-    IFLASH_BIOSIMG_HEADER* header;
+    IFLASH_BIOSIMG_HEADER* bios_header;
+    IFLASH_INI_IMG_HEADER* ini_header;
+    IFLASH_EC_IMG_HEADER* ec_header;
 
     /* Check for arguments count */
-    if (argc < 3)
+    if (argc < 2)
     {
-        printf("InsydeFlashExtractor v0.2\n\nUsage: %s INFILE OUTFILE\n", argv[0]);
+        printf(PROGRAM_NAME);
+        printf("\nUsage: %s ISFLASH.BINFILE\n", argv[0]);
         return ERR_INVALID_PARAMETER;
     }
 
@@ -113,6 +152,7 @@ int main(int argc, char* argv[])
     }
     end = in_buffer + filesize - 1;
 
+//Part 1: $_IFLASH_BIOSIMG
     /* Search for the signature in the input buffer */
     found = find_pattern(in_buffer, end, IFLASH_BIOSIMG_SIGNATURE, IFLASH_BIOSIMG_SIGNATURE_LENGTH);
     if (!found)
@@ -122,12 +162,12 @@ int main(int argc, char* argv[])
     }
 
     /* Populate the header and read used size */
-    header = (IFLASH_BIOSIMG_HEADER*) found;
+    bios_header = (IFLASH_BIOSIMG_HEADER*) found;
     found += sizeof(IFLASH_BIOSIMG_HEADER);
-    filesize = header->UsedSize;
+    filesize = bios_header->UsedSize;
 
     /* Open output file */
-    out_file = fopen(argv[2], "wb");
+    out_file = fopen("BIOSFILE.FD", "wb");
     if (!out_file)
     {
         perror("Output file can't be opened");
@@ -138,11 +178,77 @@ int main(int argc, char* argv[])
     read = fwrite(found, sizeof(char), filesize, out_file);
     if (read != filesize)
     {
-        perror("Can't write output file");
+        perror("Can't write output file BIOSFILE.FD");
         return ERR_FILE_WRITE;
     }
     
     /* Done */
-    printf("File %s successfully extracted\n", argv[2]);
+    printf("File BIOSFILE.FD successfully extracted\n");
+
+//Part 2: $_IFLASH_INI_IMG
+    /* Search for the signature in the input buffer */
+    found = find_pattern(in_buffer, end, IFLASH_INI_IMG_SIGNATURE, IFLASH_INI_IMG_SIGNATURE_LENGTH);
+    if (!found)
+    {
+        printf("Insyde platforms.ini file signature not found in input file\n");
+        return ERR_NOT_FOUND;
+    }
+
+    /* Populate the header and read used size */
+    ini_header = (IFLASH_INI_IMG_HEADER*)found;
+    found += sizeof(IFLASH_INI_IMG_HEADER);
+    filesize = ini_header->UsedSize;
+
+    /* Open output file */
+    out_file = fopen("platforms.ini", "wb");
+    if (!out_file)
+    {
+        perror("Output file can't be opened");
+        return ERR_FILE_OPEN;
+    }
+
+    /* Write INI file image to output file */
+    read = fwrite(found, sizeof(char), filesize, out_file);
+    if (read != filesize)
+    {
+        perror("Can't write output file platforms.ini");
+        return ERR_FILE_WRITE;
+    }
+
+    /* Done */
+    printf("File platforms.ini successfully extracted\n");
+
+//Part 3: $_IFLASH_EC_IMG_
+    /* Search for the signature in the input buffer */
+    found = find_pattern(in_buffer, end, IFLASH_EC_IMG_SIGNATURE, IFLASH_EC_IMG_SIGNATURE_LENGTH);
+    if (!found)
+    {
+        printf("Insyde EC image signature not found in input file\n");
+        return ERR_NOT_FOUND;
+    }
+
+    /* Populate the header and read used size */
+    ec_header = (IFLASH_EC_IMG_HEADER*)found;
+    found += sizeof(IFLASH_EC_IMG_HEADER);
+    filesize = ec_header->UsedSize;
+
+    /* Open output file */
+    out_file = fopen("EC.BIN", "wb");
+    if (!out_file)
+    {
+        perror("Output file can't be opened");
+        return ERR_FILE_OPEN;
+    }
+
+    /* Write EC image to output file */
+    read = fwrite(found, sizeof(char), filesize, out_file);
+    if (read != filesize)
+    {
+        perror("Can't write output file EC.BIN");
+        return ERR_FILE_WRITE;
+    }
+
+    /* Done */
+    printf("File EC.BIN successfully extracted\n");
     return ERR_SUCCESS;
 }
